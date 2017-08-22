f209650341b4bdb98e877081d2b6b8c2a37ce6b4
Harry Hogg <harry@hogg.io>
2017-08-22 17:47:26 +0100

    feat(BrandwatchReactAuth): Added component

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # brandwatch-react-auth
+
 BrandwatchReactAuth component for seamless application authentication
+
+### Usage
+
+The basic concept is to wrap an entire React application with the `<BrandwatchwatchReactAuth />` component, which will only render the application if the user is authenticated. When unauthenticated the user will be redirected to the authentication service, and redirected back on a successful login.
+
+```js
+render((
+  <BrandwatchReactAuth
+      audience="TOKEN_BUCKET_ID"
+      domain="TOKEN_STORE_URL"
+      onCreateStore={ (store) => {/* viziaauth store */} }>
+    <Application />
+  </BrandwatchReactAuth>
+), document.getElementById('root'))
+
+```
+
+### Demo
+
+A demo application can be run...
+
+```
+yarn install
+yarn demo
+```
+
+Open up [http://localhost:4000/](http://localhost:4000/)
---
+++ src/BrandwatchReactAuth.js
@@ -0,0 +1,73 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import jwtDecode from 'jwt-decode';
+import TokenStore from 'viziaauth';
+
+export default class BrandwatchReactAuth extends Component {
+  getChildContext() {
+    return {
+      brandwatchAuthLogout: this.handleLogout,
+      brandwatchAuthGetProfile: this.handleGetProfile,
+      brandwatchAuthGetToken: this.handleGetToken,
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    this.store = new TokenStore(this.props.domain),
+    this.handleGetProfile = this.handleGetProfile.bind(this);
+    this.handleGetToken = this.handleGetToken.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+
+    this.state = {
+      loggedIn: undefined,
+    };
+
+    props.onCreateStore(this.store);
+  }
+
+  componentWillMount() {
+    this.handleGetToken().then((token) => {
+      if (!token) {
+        return window.location.replace(this.store.loginUrl);
+      }
+
+      this.setState(() => ({ loggedIn: true }));
+    });
+  }
+
+  handleGetProfile() {
+    return this.handleGetToken().then(jwtDecode);
+  }
+
+  handleGetToken() {
+    return this.store.getToken({ aud: this.props.audience });
+  }
+
+  handleLogout() {
+    return this.store.removeToken({ aud: this.props.audience }).then(() => {
+      window.location.replace(this.store.loginUrl);
+    });
+  }
+
+  render() {
+    if (this.state.loggedIn === true) {
+      return this.props.children
+    }
+
+    return null;
+  }
+}
+
+
+BrandwatchReactAuth.propTypes = {
+  audience: PropTypes.string.isRequired,
+  domain: PropTypes.string.isRequired,
+  onCreateStore: PropTypes.func,
+}
+
+BrandwatchReactAuth.childContextTypes = {
+  brandwatchAuthGetProfile: PropTypes.func.isRequired,
+  brandwatchAuthGetToken: PropTypes.func.isRequired,
+  brandwatchAuthLogout: PropTypes.func.isRequired,
+};
